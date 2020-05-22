7c2c616d4d24398cd95057a8a0312e75d5741185
Shaun Millar <shaun.millar@nttdata.com>
2020-05-22 14:13:28 -0700

    first commit

---
+++ src/main/java/my/pkg/name/MyObject.java
@@ -0,0 +1,16 @@
+package my.pkg.name;
+
+/**
+ * MyObject
+ *
+ */
+public class MyObject {
+	
+	public MyObject() {};
+	
+	
+	public String getGreeting() {
+		return "Hello World, v0.2.0";
+	}
+
+}
